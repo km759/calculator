21d68c189a167f2458641397c509601513c5bd42
km759 <km759@njit.edu>
2021-02-20 19:05:23 -0500

    feature: subtraction csv test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,5 +1,7 @@
 import unittest
 from Calculator import Calculator
+from CsvReader import  CsvReader
+from pprint import pprint
 
 class MyTestCase(unittest.TestCase):
 
@@ -18,9 +20,11 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.add(2,2), 4)
         self.assertEqual(self.calculator.result, 4)
 
-    def test_subtract_method_calculator(self):
-        self.assertEqual(self.calculator.subtract(2,2), 0)
-        self.assertEqual(self.calculator.result, 0)
+    def test_subtraction(self):
+        test_data = CsvReader('/src/Unit Test Subtraction.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
 
 if __name__ == '__main__':
